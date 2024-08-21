170fbb9c3751be7ce3b01701166b1f53e91e6596
Jeonghoon2 <ukidd12@naver.com>
2024-08-21 12:25:23 +0900

    tqdm

--- src/kafkachat/kafka/pro.py
+++ src/kafkachat/kafka/pro.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 import time
 import json
-
+import tqdm
 pro = KafkaProducer(
         bootstrap_servers=['localhost:9092'],
         value_serializer=lambda x:json.dumps(x).encode('utf-8')
@@ -9,7 +9,7 @@ pro = KafkaProducer(
 
 start = time.time()
 
-for i in range(10):
+for i in tqdm(range(10)):
     data = {'str': 'value' + str(i)}
     pro.send('topic1', value=data)
     pro.flush()
